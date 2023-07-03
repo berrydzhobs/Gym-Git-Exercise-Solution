39c558a372999328ed1f649fe2a8e93923d50df7
Берри Джобс <105468324+berrydzhobs@users.noreply.github.com>
2023-07-03 20:21:22 +0200

    Second Commit

---
+++ exc/app.js
@@ -0,0 +1,27 @@
+/*
+
+document.getElementById('');
+document.getElementsByClassName('');
+document.getElementsByTagName('');
+document.querySelector('');
+document.querySelectorAll('');
+
+*/
+
+
+/*
+const newChat = document.querySelector('#newchat');
+
+function changeColorNewChat() {
+  newChat.style.backgroundColor = "red";
+}
+
+if (newChat) {
+  newChat.addEventListener("mouseover", changeColorNewChat);
+}
+*/
+
+
+const fruitsDiv = document.querySelector('li');
+
+console.log('Hello' + fruitsDiv)
